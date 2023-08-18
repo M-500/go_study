693ea50d9fd84c47b03d3e124dab1a551fc43b06
wulinlin <18574945291@163.com>
2023-08-18 16:27:58 +0800

    grpc TLS 调通

--- grpc_study/grpc-tls/client/clientTLS/client.go
+++ grpc_study/grpc-tls/client/clientTLS/client.go
@@ -1,1 +1,37 @@
-package clientTLS
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"grpc-tls/pb/tls_demo"
+	"log"
+)
+
+const port = ":8999"
+
+func main() {
+	// 根据客户端输入的证书文件和密钥构造 TLS 凭证。
+	// 第二个参数 serverNameOverride 为服务名称。
+	c, err := credentials.NewClientTLSFromFile("./conf/server.pem", "grpc-tls")
+
+	if err != nil {
+		log.Fatalf("credentials.NewClientTLSFromFile err: %v", err)
+	}
+	// 返回一个配置连接的 DialOption 选项。
+	// 用于 grpc.Dial(target string, opts ...DialOption) 设置连接选项
+	conn, err := grpc.Dial(port, grpc.WithTransportCredentials(c))
+	if err != nil {
+		log.Fatalf("grpc.Dial err: %v", err)
+	}
+	defer conn.Close()
+	client := tls_demo.NewHelloServiceClient(conn)
+	resp, err := client.SayHello(context.Background(), &tls_demo.HelloRequest{
+		Name: "gRPC",
+	})
+	if err != nil {
+		log.Fatalf("client.Search err: %v", err)
+	}
+
+	log.Printf("resp: %s", resp.GetRes())
+}
--- grpc_study/grpc-tls/server/serverTLS/server.go
+++ grpc_study/grpc-tls/server/serverTLS/server.go
@@ -1,1 +1,44 @@
-package serverTLS
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"grpc-tls/pb/tls_demo"
+	"log"
+	"net"
+	"time"
+)
+
+const port = ":8999"
+
+type TLSService struct {
+	tls_demo.UnimplementedHelloServiceServer
+}
+
+func (t *TLSService) SayHello(ctx context.Context, in *tls_demo.HelloRequest) (*tls_demo.HelloResponse, error) {
+	format := time.Now().Format("2006-01-02 15:04:05")
+	return &tls_demo.HelloResponse{Res: "您好 " + in.GetName() + "---" + format}, nil
+}
+
+func main() {
+	listen, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("credentials.NewServerTLSFromFile err: %v", err)
+	}
+
+	cred, err := credentials.NewServerTLSFromFile("./conf/server.pem", "./conf/server.key")
+	if err != nil {
+		log.Fatalf("credentials.NewServerTLSFromFile err: %v", err)
+	}
+
+	s := grpc.NewServer(grpc.Creds(cred))
+	tls_demo.RegisterHelloServiceServer(s, &TLSService{})
+
+	err = s.Serve(listen)
+	if err != nil {
+		log.Fatalf("close err:%v", err)
+		return
+	}
+
+}
